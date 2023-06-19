e2f23ccf1b75a62756226f4b956c48c8d503c269
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-06-19 10:20:09 -0300

    Atualizado modulo Gamuza_Basic: adicionado configuração para validação do celular do cliente

--- app/code/local/Gamuza/Basic/Model/Customer/Customer.php
+++ app/code/local/Gamuza/Basic/Model/Customer/Customer.php
@@ -20,6 +20,8 @@ class Gamuza_Basic_Model_Customer_Customer
 
     public const XML_PATH_GENERATE_HUMAN_FRIENDLY_EMAIL = 'customer/create_account/generate_human_friendly_email';
 
+    public const XML_PATH_VALIDATE_CUSTOMER_CELLPHONE = 'customer/address/validate_cellphone';
+
     /**
      * Send corresponding email template
      *
