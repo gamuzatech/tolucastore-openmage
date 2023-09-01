e13e17c14fc95d952e37206e9da1586f492f7de5
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-08-31 23:43:03 -0300

    Atualizado modulo Gamuza_Basic: adicionado trait para validar se produtos do carrinho pertencem a determinado conjunto de atributos configurado no painel admin

--- app/code/local/Gamuza/Basic/Model/Shipping/Carrier/Tablerate.php
+++ app/code/local/Gamuza/Basic/Model/Shipping/Carrier/Tablerate.php
@@ -8,56 +8,9 @@
 class Gamuza_Basic_Model_Shipping_Carrier_Tablerate
     extends Mage_Shipping_Model_Carrier_Tablerate
 {
+    use Gamuza_Basic_Trait_Shipping_Carrier_Abstract;
+
     const ADMIN_WEBSITE_ID = 0;
-
-    /**
-     * Collect and get rates
-     *
-     * @param Mage_Shipping_Model_Rate_Request $request
-     * @return false|Mage_Shipping_Model_Rate_Result
-     */
-    public function collectRates(Mage_Shipping_Model_Rate_Request $request)
-    {
-        if (!$this->getConfigFlag('active'))
-        {
-            return false;
-        }
-
-        $attributeSets = $this->getConfigData('attribute_set');
-
-        if (!empty($attributeSets))
-        {
-            $attributeSets = explode(',', $attributeSets);
-
-            $itemsCount = 0;
-
-            foreach ($request->getAllItems() as $item)
-            {
-                if (!in_array($item->getProduct()->getAttributeSetId(), $attributeSets))
-                {
-                    $itemsCount ++;
-                }
-            }
-
-            if ($itemsCount > 0 && $this->getConfigFlag('showmethod'))
-            {
-                $error = Mage::getModel('shipping/rate_result_error')
-                    ->setCarrier($this->_code)
-                    ->setCarrierTitle($this->getConfigData('title'))
-                    ->setErrorMessage($this->getConfigData('specificerrmsg')
-                        ?: Mage::helper('shipping')->__('The shipping module is not available for selected delivery country.'))
-                ;
-
-                return $error;
-            }
-            else if ($itemsCount > 0)
-            {
-                return false;
-            }
-        }
-
-        return parent::collectRates($request);
-    }
 
     /**
      * Get Rate
