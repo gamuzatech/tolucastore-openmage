3e78a533c19d43725a5c9934be668b2434ec91b7
Eneias Ramos de Melo <eneias@gamuza.com.br>
2023-08-31 23:43:03 -0300

    Atualizado modulo Gamuza_Basic: adicionado trait para validar se produtos do carrinho pertencem a determinado conjunto de atributos configurado no painel admin

---
+++ app/code/local/Gamuza/Basic/Model/Shipping/Carrier/Flatrate.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package     Gamuza_Basic
+ * @copyright   Copyright (c) 2023 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+class Gamuza_Basic_Model_Shipping_Carrier_Flatrate
+    extends Mage_Shipping_Model_Carrier_Flatrate
+{
+    use Gamuza_Basic_Trait_Shipping_Carrier_Abstract;
+}
+
