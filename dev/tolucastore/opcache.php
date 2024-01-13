743c3cbd6ab1abf4f4217275d90e17260ace5652
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-01-13 13:17:16 -0300

    Atualizado modulo Gamuza_Basic: adicionado dev/tolucastore/opcache.php

---
+++ dev/tolucastore/opcache.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package     Gamuza_Basic
+ * @copyright   Copyright (c) 2024 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+var_dump('<pre>', opcache_get_status(true));
+
