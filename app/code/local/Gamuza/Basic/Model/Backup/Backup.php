81b6afc8568c5a7f352ffcfe2b857a6c1e45ce40
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-03-27 02:05:22 -0300

    Atualizado modulo Gamuza_Basic: adicionado rewrite para Mage_Backup_Model_Backup para reduzir COMPRESS_RATE

---
+++ app/code/local/Gamuza/Basic/Model/Backup/Backup.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package     Gamuza_Basic
+ * @copyright   Copyright (c) 2024 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+/**
+ * Backup file item model
+ */
+class Gamuza_Basic_Model_Backup_Backup extends Mage_Backup_Model_Backup
+{
+    /* internal constants */
+    public const COMPRESS_RATE = 6;
+}
+
