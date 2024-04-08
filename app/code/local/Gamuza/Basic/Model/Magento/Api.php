c4df41b632b4ddb39d1c77770fad0d64ee94884b
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-04-08 13:37:51 -0300

    Atualizado modulo Gamuza_Basic: adicionado evento basic_magento_api_info para apiPath basic_magento.info

--- app/code/local/Gamuza/Basic/Model/Magento/Api.php
+++ app/code/local/Gamuza/Basic/Model/Magento/Api.php
@@ -21,7 +21,8 @@ class Gamuza_Basic_Model_Magento_Api extends Mage_Core_Model_Magento_Api
 
         $result['tolucastore_version'] = Mage::getOpenMageVersion();
         $result['enable_pdv_admin_dashboard'] = Mage::getStoreConfigFlag('admin/dashboard/enable_pdv');
-        $result['brazil'] = Mage::getStoreConfig('brazil');
+
+        Mage::dispatchEvent ('basic_magento_api_info', array ('info' => & $result));
 
         return $result;
     }
