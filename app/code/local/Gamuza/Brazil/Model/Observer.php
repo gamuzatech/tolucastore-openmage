9d411820976634aa93093235a0c0d064ecab0948
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-04-08 19:34:47 -0300

    Atualizado modulo Gamuza_Brazil: ajustado campos system.xml

--- app/code/local/Gamuza/Brazil/Model/Observer.php
+++ app/code/local/Gamuza/Brazil/Model/Observer.php
@@ -7,6 +7,53 @@
 
 class Gamuza_Brazil_Model_Observer
 {
+    public function basicMagentoApiInfo (Varien_Event_Observer $observer)
+    {
+        $event = $observer->getEvent ();
+        $info = $event->getInfo ();
+
+        $certificateDirectory = Mage::getBaseDir ('var') . DS . 'cert' . DS . 'brazil';
+        $certificateFilename  = Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_CERTIFICATE_FILENAME);
+        $certificateReadable  = is_readable ($certificateDirectory . DS . $certificateFilename);
+        $certificateContents  = $certificateReadable ? file_get_contents ($certificateDirectory . DS . $certificateFilename) : null;
+
+        $info['brazil'] = array(
+            'certificate' => array(
+                'type'      => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_CERTIFICATE_TYPE)),
+                'directory' => $certificateDirectory,
+                'filename'  => $certificateFilename,
+                'readable'  => $certificateReadable,
+                // 'contents'  => base64_encode ($certificateContents),
+                'password'  => Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_CERTIFICATE_PASSWORD),
+            ),
+            'csc' => array(
+                'id'   => Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_CSC_ID),
+                'code' => Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_CSC_CODE),
+            ),
+            'setting' => array(
+                'environment_id' => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_ENVIRONMENT_ID)),
+                'version_id'     => strval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_VERSION_ID)),
+                'country_id'     => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_COUNTRY_ID)),
+                'region_id'      => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_REGION_ID)),
+                'city_id'        => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_CITY_ID)),
+            ),
+            'nfe' => array(
+                'batch_id'  => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFE_BATCH_ID)),
+                'number_id' => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFE_NUMBER_ID)),
+                'model_id'  => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFE_MODEL_ID)),
+                'series_id' => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFE_SERIES_ID)),
+            ),
+            'nfce' => array(
+                'batch_id'  => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_BATCH_ID)),
+                'number_id' => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_NUMBER_ID)),
+                'model_id'  => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_MODEL_ID)),
+                'series_id' => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_SERIES_ID)),
+            ),
+        );
+
+        $event->setInfo($info);
+    }
+
     public function salesQuoteItemSetProduct (Varien_Event_Observer $observer)
     {
         $quoteItem = $observer->getQuoteItem ();
