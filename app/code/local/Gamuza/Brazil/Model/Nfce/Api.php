fb28c3278afb0e3ec2f91af5a6adc4eea05abe6c
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-04-08 21:20:30 -0300

    Atualizado modulo Gamuza_Brazil: ajustado apiPath brazil_nfce.create

--- app/code/local/Gamuza/Brazil/Model/Nfce/Api.php
+++ app/code/local/Gamuza/Brazil/Model/Nfce/Api.php
@@ -10,29 +10,6 @@
  */
 class Gamuza_Brazil_Model_Nfce_Api extends Mage_Api_Model_Resource_Abstract
 {
-    const XML_PATH_BRAZIL_NFCE_ENVIRONMENT = Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_ENVIRONMENT;
-    const XML_PATH_BRAZIL_NFCE_VERSION     = Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_VERSION;
-
-    const XML_PATH_BRAZIL_NFCE_MODEL  = Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_MODEL;
-    const XML_PATH_BRAZIL_NFCE_SERIES = Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_SERIES;
-
-    const XML_PATH_BRAZIL_NFCE_BATCH_ID  = Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_BATCH_ID;
-    const XML_PATH_BRAZIL_NFCE_REGION_ID = Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_REGION_ID;
-    const XML_PATH_BRAZIL_NFCE_CITY_ID   = Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFCE_CITY_ID;
-
-    const NFE_PRINT_PORTRAIT      = Gamuza_Brazil_Helper_Data::NFE_PRINT_PORTRAIT;
-    const NFE_EMISSION_NORMAL     = Gamuza_Brazil_Helper_Data::NFE_EMISSION_NORMAL;
-    const NFE_FINALITY_NORMAL     = Gamuza_Brazil_Helper_Data::NFE_FINALITY_NORMAL;
-    const NFE_CONSUMER_FINAL      = Gamuza_Brazil_Helper_Data::NFE_CONSUMER_FINAL;
-    const NFE_PRESENCE_DELIVERY   = Gamuza_Brazil_Helper_Data::NFE_PRESENCE_DELIVERY;
-    const NFE_INTERMEDIARY_OWN    = Gamuza_Brazil_Helper_Data::NFE_INTERMEDIARY_OWN;
-    const NFE_PROCESS_PDV         = Gamuza_Brazil_Helper_Data::NFE_PROCESS_PDV;
-    const NFE_FREIGHT_EMITTER     = Gamuza_Brazil_Helper_Data::NFE_FREIGHT_EMITTER;
-    const NFE_CRT_SIMPLE_NATIONAL = Gamuza_Brazil_Helper_Data::NFE_CRT_SIMPLE_NATIONAL;
-    const NFE_CUSTOMER_IE_NONE    = Gamuza_Brazil_Helper_Data::NFE_CUSTOMER_IE_NONE;
-
-    const NFE_PAYMENT_TYPE_MONEY = Gamuza_Brazil_Helper_Data::NFE_PAYMENT_TYPE_MONEY;
-
     /**
      * Attributes map array per entity type
      *
@@ -48,11 +25,6 @@ class Gamuza_Brazil_Model_Nfce_Api extends Mage_Api_Model_Resource_Abstract
 
         $this->_shippingCountryId = Mage::getStoreConfig (Mage_Shipping_Model_Config::XML_PATH_ORIGIN_COUNTRY_ID);
         $this->_shippingRegionId  = Mage::getStoreConfig (Mage_Shipping_Model_Config::XML_PATH_ORIGIN_REGION_ID);
-
-        $this->_fantasyName   = Mage::getStoreConfig ('general/store_information/name');
-        $this->_companyTaxvat = Mage::getStoreConfig ('general/store_information/taxvat');
-        $this->_companyName   = Mage::getStoreConfig ('general/store_information/company_name');
-        $this->_companyIE     = Mage::getStoreConfig ('general/store_information/company_ie');
     }
 
     public function items ($filters = null)
@@ -99,10 +71,6 @@ class Gamuza_Brazil_Model_Nfce_Api extends Mage_Api_Model_Resource_Abstract
 
         foreach ($collection as $nfce)
         {
-            $nfce->setFantasyName ($this->_fantasyName)
-                ->setCompanyName ($this->_companyName)
-            ;
-
             $result [] = array (
                 'entity_id'       => intval ($nfce->getId ()),
                 'order_id'        => intval ($nfce->getOrderId ()),
@@ -166,7 +134,7 @@ class Gamuza_Brazil_Model_Nfce_Api extends Mage_Api_Model_Resource_Abstract
         return $result;
     }
 
-    public function create ($orderIncrementId, $orderProtectCode, $operationId, $operationName)
+    public function create ($orderIncrementId, $orderProtectCode, $data = array ())
     {
         if (empty ($orderIncrementId))
         {
@@ -178,9 +146,9 @@ class Gamuza_Brazil_Model_Nfce_Api extends Mage_Api_Model_Resource_Abstract
             $this->_fault ('code_not_specified');
         }
 
-        if (empty ($operationId) || empty ($operationName))
+        if (empty ($data))
         {
-            $this->_fault ('operation_not_specified');
+            $this->_fault ('data_not_specified');
         }
 
         $order = $this->_initOrder ($orderIncrementId, $orderProtectCode);
@@ -215,35 +183,14 @@ class Gamuza_Brazil_Model_Nfce_Api extends Mage_Api_Model_Resource_Abstract
         $polynomial = hash ('crc32b', $order->getId ());
 
         $nfce = Mage::getModel ('brazil/nfce')
+            ->addData ($data)
             ->setOrderId ($order->getId ())
             ->setStoreId ($order->getStoreId ())
             ->setCustomerId ($order->getCustomerId ())
-            ->setEnvironmentId (Mage::getStoreConfig (self::XML_PATH_BRAZIL_NFCE_ENVIRONMENT))
-            ->setVersionId (Mage::getStoreConfig (self::XML_PATH_BRAZIL_NFCE_VERSION))
-            ->setModelId (Mage::getStoreConfig (self::XML_PATH_BRAZIL_NFCE_MODEL))
-            ->setSeriesId (Mage::getStoreConfig (self::XML_PATH_BRAZIL_NFCE_SERIES))
-            ->setBatchId (Mage::getStoreConfig (self::XML_PATH_BRAZIL_NFCE_BATCH_ID))
-            ->setRegionId (Mage::getStoreConfig (self::XML_PATH_BRAZIL_NFCE_REGION_ID))
-            ->setCityId (Mage::getStoreConfig (self::XML_PATH_BRAZIL_NFCE_CITY_ID))
             ->setNumberId (Mage::helper ('brazil')->getNumberId ('nfce', array ('order_id' => $order->getId ())))
-            ->setOperationId ($operationId)
-            ->setDestinyId ($destinyId)
-            ->setPrintId (self::NFE_PRINT_PORTRAIT)
-            ->setEmissionId (self::NFE_EMISSION_NORMAL)
-            ->setFinalityId (self::NFE_FINALITY_NORMAL)
-            ->setConsumerId (self::NFE_CONSUMER_FINAL)
-            ->setPresenceId (self::NFE_PRESENCE_DELIVERY)
-            ->setIntermediaryId (self::NFE_INTERMEDIARY_OWN)
-            ->setProcessId (self::NFE_PROCESS_PDV)
-            ->setFreightId (self::NFE_FREIGHT_EMITTER)
-            ->setCrtId (self::NFE_CRT_SIMPLE_NATIONAL)
-            ->setOperation ($operationName)
             ->setCode (hexdec ($polynomial))
-            ->setCompanyTaxvat ($this->_companyTaxvat)
-            ->setCompanyIe ($this->_companyIE)
             ->setCustomerTaxvat ($order->getCustomerTaxvat ())
             ->setCustomerEmail ($order->getCustomerEmail ())
-            ->setCustomerIe (self::NFE_CUSTOMER_IE_NONE)
             ->setCreatedAt (date ('c'))
         ;
 
@@ -267,7 +214,7 @@ class Gamuza_Brazil_Model_Nfce_Api extends Mage_Api_Model_Resource_Abstract
 
         switch ($order->getPayment ()->getMethod ())
         {
-            case 'cashondelivery': { $paymentMethod = self::NFE_PAYMENT_TYPE_MONEY; break; }
+            case 'cashondelivery': { $paymentMethod = Gamuza_Brazil_Helper_Data::NFE_PAYMENT_TYPE_MONEY; break; }
         }
 
         $nfce->setPaymentAmount ($paymentAmount)
@@ -276,10 +223,6 @@ class Gamuza_Brazil_Model_Nfce_Api extends Mage_Api_Model_Resource_Abstract
         ;
 
     __return:
-
-        $nfce->setFantasyName ($this->_fantasyName)
-            ->setCompanyName ($this->_companyName)
-        ;
 
         $result = array (
             'entity_id'       => intval ($nfce->getId ()),
