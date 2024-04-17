6490865164d6684f313ef54a6e0aea2ac2f2d08e
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-04-17 16:16:08 -0300

    Atualizado modulo Gamuza_Brazil: ajustado opções para NFe e NFC-e

--- app/code/local/Gamuza/Brazil/Model/Adminhtml/System/Config/Source/Nfe/Emission.php
+++ app/code/local/Gamuza/Brazil/Model/Adminhtml/System/Config/Source/Nfe/Emission.php
@@ -18,13 +18,14 @@ class Gamuza_Brazil_Model_Adminhtml_System_Config_Source_Nfe_Emission
     public function toOptionArray()
     {
         $result = array(
-            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_NORMAL,             'label' => Mage::helper ('brazil')->__('Normal')),
-            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_FS,     'label' => Mage::helper ('brazil')->__('Contingency FS')),
-            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SCAN,   'label' => Mage::helper ('brazil')->__('Contingency SCAN')),
-            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_DPEC,   'label' => Mage::helper ('brazil')->__('Contingency DPEC')),
-            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_FS_DA,  'label' => Mage::helper ('brazil')->__('Contingency FS-DA')),
-            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SVC_AN, 'label' => Mage::helper ('brazil')->__('Contingency SVC-AN')),
-            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SVC_RS, 'label' => Mage::helper ('brazil')->__('Contingency SVC-RS')),
+            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_NORMAL,              'label' => Mage::helper ('brazil')->__('Normal')),
+            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_FS_IA,   'label' => Mage::helper ('brazil')->__('Contingency FS-IA')),
+            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SCAN,    'label' => Mage::helper ('brazil')->__('Contingency SCAN')),
+            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_DPEC,    'label' => Mage::helper ('brazil')->__('Contingency DPEC')),
+            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_FS_DA,   'label' => Mage::helper ('brazil')->__('Contingency FS-DA')),
+            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SVC_AN,  'label' => Mage::helper ('brazil')->__('Contingency SVC-AN')),
+            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SVC_RS,  'label' => Mage::helper ('brazil')->__('Contingency SVC-RS')),
+            array ('value' => Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_OFFLINE, 'label' => Mage::helper ('brazil')->__('Contingency Offline')),
         );
 
         return $result;
@@ -38,13 +39,14 @@ class Gamuza_Brazil_Model_Adminhtml_System_Config_Source_Nfe_Emission
     public function toArray()
     {
         $result = array(
-            Gamuza_Brazil_Helper_Data::NFE_EMISSION_NORMAL             => Mage::helper ('brazil')->__('Normal'),
-            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_FS     => Mage::helper ('brazil')->__('Contingency FS'),
-            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SCAN   => Mage::helper ('brazil')->__('Contingency SCAN'),
-            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_DPEC   => Mage::helper ('brazil')->__('Contingency DPEC'),
-            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_FS_DA  => Mage::helper ('brazil')->__('Contingency FS-DA'),
-            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SVC_AN => Mage::helper ('brazil')->__('Contingency SVC-AN'),
-            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SVC_RS => Mage::helper ('brazil')->__('Contingency SVC-RS'),
+            Gamuza_Brazil_Helper_Data::NFE_EMISSION_NORMAL              => Mage::helper ('brazil')->__('Normal'),
+            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_FS_IA   => Mage::helper ('brazil')->__('Contingency FS-IA'),
+            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SCAN    => Mage::helper ('brazil')->__('Contingency SCAN'),
+            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_DPEC    => Mage::helper ('brazil')->__('Contingency DPEC'),
+            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_FS_DA   => Mage::helper ('brazil')->__('Contingency FS-DA'),
+            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SVC_AN  => Mage::helper ('brazil')->__('Contingency SVC-AN'),
+            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_SVC_RS  => Mage::helper ('brazil')->__('Contingency SVC-RS'),
+            Gamuza_Brazil_Helper_Data::NFE_EMISSION_CONTINGENCY_OFFLINE => Mage::helper ('brazil')->__('Contingency Offline'),
         );
 
         return $result;
