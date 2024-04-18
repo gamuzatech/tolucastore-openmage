8f0bc7eb853b245a25de1b3f6b4a6245c360f068
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-04-18 13:39:23 -0300

    Atualizado modulo Gamuza_Brazil: adicionado informações de loja no apiPath basic_magento.info

--- app/code/local/Gamuza/Brazil/Model/Observer.php
+++ app/code/local/Gamuza/Brazil/Model/Observer.php
@@ -29,12 +29,14 @@ class Gamuza_Brazil_Model_Observer
                 'code' => Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_CSC_CODE),
             ),
             'setting' => array(
-                'crt_id'         => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_CRT_ID)),
                 'environment_id' => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_ENVIRONMENT_ID)),
                 'version_id'     => strval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_VERSION_ID)),
                 'country_id'     => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_COUNTRY_ID)),
                 'region_id'      => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_REGION_ID)),
                 'city_id'        => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_CITY_ID)),
+                'crt_id'         => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_CRT_ID)),
+                'company_ie'     => Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_COMPANY_IE),
+                'company_im'     => Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_SETTING_COMPANY_IM),
             ),
             'nfe' => array(
                 'print_id'  => intval (Mage::getStoreConfig (Gamuza_Brazil_Helper_Data::XML_PATH_BRAZIL_NFE_PRINT_ID)),
