16d3c836718951a9052de8636dc1d9cbfb4cf4f4
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-05-19 21:05:59 -0300

    Atualizado modulo Gamuza_Brazil: ajustado listagem para NCM no cadastro do produto

--- app/code/local/Gamuza/Brazil/Model/Catalog/Product/Attribute/Source/Ncm.php
+++ app/code/local/Gamuza/Brazil/Model/Catalog/Product/Attribute/Source/Ncm.php
@@ -9,5 +9,26 @@ class Gamuza_Brazil_Model_Catalog_Product_Attribute_Source_Ncm
     extends Gamuza_Brazil_Model_Catalog_Product_Attribute_Source_Abstract
 {
     const FILENAME = 'ncm.csv';
+
+    public function getAllOptions($withEmpty = true, $defaultValues = false)
+    {
+        if ($this->_options === null)
+        {
+            $this->_options = array (
+                array ('value' => 0, 'label' => Mage::helper ('core')->__('-- Please Select --')),
+            );
+
+            $collection = Mage::getModel ('brazil/ibpt')->getCollection ()
+                ->addFieldToFilter ('type', array ('eq' => Gamuza_Brazil_Helper_Data::IBPT_TYPE_NCM))
+            ;
+
+            foreach ($collection as $ibpt)
+            {
+                $this->_options [] = array ('value' => $ibpt->getCode (), 'label' => sprintf ('%s %s', $ibpt->getCode (), $ibpt->getDescription ()));
+            }
+        }
+
+        return $this->_options;
+    }
 }
 
