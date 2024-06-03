a4416e320b3a1ff88c9a648ac7673df42d377297
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-06-03 20:51:11 -0300

    Atualizado modulo Gamuza_Brazil: adicionado suporte a tabela CEST

--- app/code/local/Gamuza/Brazil/Block/Adminhtml/Cest/Grid.php
+++ app/code/local/Gamuza/Brazil/Block/Adminhtml/Cest/Grid.php
@@ -5,13 +5,13 @@
  * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
  */
 
-class Gamuza_Brazil_Block_Adminhtml_Ibpt_Grid extends Mage_Adminhtml_Block_Widget_Grid
+class Gamuza_Brazil_Block_Adminhtml_Cest_Grid extends Mage_Adminhtml_Block_Widget_Grid
 {
 	public function __construct ()
 	{
 		parent::__construct ();
 
-		$this->setId ('brazilIbptGrid');
+		$this->setId ('brazilCestGrid');
 		$this->setDefaultSort ('entity_id');
 		$this->setDefaultDir ('DESC');
 		$this->setSaveParametersInSession (true);
@@ -26,7 +26,7 @@ class Gamuza_Brazil_Block_Adminhtml_Ibpt_Grid extends Mage_Adminhtml_Block_Widge
 
 	protected function _prepareCollection ()
 	{
-		$collection = Mage::getModel ('brazil/ibpt')->getCollection ();
+		$collection = Mage::getModel ('brazil/cest')->getCollection ();
 
 		$this->setCollection ($collection);
 
@@ -50,67 +50,15 @@ class Gamuza_Brazil_Block_Adminhtml_Ibpt_Grid extends Mage_Adminhtml_Block_Widge
 		    'align'   => 'right',
 		    'index'   => 'code',
 		));
-		$this->addColumn ('exception', array(
-		    'header'  => Mage::helper ('brazil')->__('Exception'),
-		    'align'   => 'right',
-		    'index'   => 'exception',
-		));
-		$this->addColumn ('type', array(
-		    'header'  => Mage::helper ('brazil')->__('Type'),
-		    'align'   => 'right',
-            'width'   => '50px',
-		    'index'   => 'type',
-            'type'    => 'options',
-            'options' => Mage::getModel ('brazil/adminhtml_system_config_source_ibpt_type')->toArray (),
-		));
 		$this->addColumn ('description', array(
 		    'header'  => Mage::helper ('brazil')->__('Description'),
 		    'align'   => 'right',
 		    'index'   => 'description',
 		));
-		$this->addColumn ('national_federal', array(
-		    'header'  => Mage::helper ('brazil')->__('National Federal'),
-		    'align'   => 'right',
-            'type'    => 'price',
-		    'index'   => 'national_federal',
-            'currency_code' => $store->getBaseCurrency ()->getCode (),
-		));
-		$this->addColumn ('imported_federal', array(
-		    'header'  => Mage::helper ('brazil')->__('Imported Federal'),
-		    'align'   => 'right',
-            'type'    => 'price',
-		    'index'   => 'imported_federal',
-            'currency_code' => $store->getBaseCurrency ()->getCode (),
-		));
-		$this->addColumn ('state', array(
-		    'header'  => Mage::helper ('brazil')->__('State'),
-		    'align'   => 'right',
-            'type'    => 'price',
-		    'index'   => 'state',
-            'currency_code' => $store->getBaseCurrency ()->getCode (),
-		));
-		$this->addColumn ('local', array(
-		    'header'  => Mage::helper ('brazil')->__('Local'),
-		    'align'   => 'right',
-            'type'    => 'price',
-		    'index'   => 'local',
-            'currency_code' => $store->getBaseCurrency ()->getCode (),
-		));
-		$this->addColumn ('key', array(
-		    'header'  => Mage::helper ('brazil')->__('Key'),
-		    'align'   => 'right',
-            'type'    => 'number',
-		    'index'   => 'key',
-		));
 		$this->addColumn ('version', array(
 		    'header'  => Mage::helper ('brazil')->__('Version'),
 		    'align'   => 'right',
 		    'index'   => 'version',
-		));
-		$this->addColumn ('source', array(
-		    'header'  => Mage::helper ('brazil')->__('Source'),
-		    'align'   => 'right',
-		    'index'   => 'source',
 		));
 		$this->addColumn ('begin_at', array(
 			'header' => Mage::helper ('brazil')->__('Begin At'),
