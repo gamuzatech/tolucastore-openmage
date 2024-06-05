f7c671bc0a4697e71f6231f7a115a7ee030c8c2f
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-06-05 02:31:49 -0300

    Atualizado modulo Gamuza_Brazil: adicionado tabela de Países IBGE

--- app/code/local/Gamuza/Brazil/Model/Adminhtml/System/Config/Source/Nfe/Crt.php
+++ app/code/local/Gamuza/Brazil/Model/Adminhtml/System/Config/Source/Nfe/Crt.php
@@ -21,9 +21,7 @@ class Gamuza_Brazil_Model_Adminhtml_System_Config_Source_Nfe_Crt
             array ('value' => Gamuza_Brazil_Helper_Data::NFE_CRT_SIMPLE_NATIONAL,        'label' => Mage::helper ('brazil')->__('Simple National')),
             array ('value' => Gamuza_Brazil_Helper_Data::NFE_CRT_SIMPLE_NATIONAL_EXCESS, 'label' => Mage::helper ('brazil')->__('Simple National Excess')),
             array ('value' => Gamuza_Brazil_Helper_Data::NFE_CRT_NORMAL_REGIME,          'label' => Mage::helper ('brazil')->__('Normal Regime')),
-            /*
             array ('value' => Gamuza_Brazil_Helper_Data::NFE_CRT_SIMPLE_NATIONAL_MEI,    'label' => Mage::helper ('brazil')->__('Simple National MEI')),
-            */
         );
 
         return $result;
@@ -40,9 +38,7 @@ class Gamuza_Brazil_Model_Adminhtml_System_Config_Source_Nfe_Crt
             Gamuza_Brazil_Helper_Data::NFE_CRT_SIMPLE_NATIONAL        => Mage::helper ('brazil')->__('Simple National'),
             Gamuza_Brazil_Helper_Data::NFE_CRT_SIMPLE_NATIONAL_EXCESS => Mage::helper ('brazil')->__('Simple National Excess'),
             Gamuza_Brazil_Helper_Data::NFE_CRT_NORMAL_REGIME          => Mage::helper ('brazil')->__('Normal Regime'),
-            /*
             Gamuza_Brazil_Helper_Data::NFE_CRT_SIMPLE_NATIONAL_MEI    => Mage::helper ('brazil')->__('Simple National MEI'),
-            */
         );
 
         return $result;
