d5ca9751c4c88275c4861f1ca3a52f6cdf68d162
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-06-05 19:12:03 -0300

    Atualizado modulo Gamuza_Brazil: adicionado tabela de Cidades IBGE

---
+++ app/code/local/Gamuza/Brazil/Model/Adminhtml/System/Config/Source/Ibge/City.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Gamuza_Brazil
+ * @copyright   Copyright (c) 2024 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+/**
+ * Used in creating options for Ibge_City config value selection
+ */
+class Gamuza_Brazil_Model_Adminhtml_System_Config_Source_Ibge_City
+    extends Gamuza_Brazil_Model_Adminhtml_System_Config_Source_Ibge_Abstract
+{
+    public const FIELD = 'name';
+
+    public function _getCollection ()
+    {
+        return Mage::getModel ('brazil/city')->getCollection ();
+    }
+}
+
