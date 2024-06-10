2f1370ef0d1c9dffc78fd04eaa6287addce3c57a
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-06-10 12:04:13 -0300

    Atualizado modulo Gamuza_Brazil: adicionado tabela de respostas para NFC-e

---
+++ app/code/local/Gamuza/Brazil/sql/brazil_setup/mysql4-upgrade-0.0.11-0.0.12.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * @package     Gamuza_Brazil
+ * @copyright   Copyright (c) 2024 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+$installer = $this;
+$installer->startSetup ();
+
+function addBrazilNfceResponseTable ($installer, $model, $description)
+{
+    $table = $installer->getTable ($model);
+
+    $sqlBlock = <<< SQLBLOCK
+CREATE TABLE IF NOT EXISTS {$table}
+(
+    entity_id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (entity_id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 COMMENT='{$description}';
+SQLBLOCK;
+
+    $installer->run ($sqlBlock);
+
+    $installer->getConnection ()
+        ->addColumn ($table, 'result_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => true,
+            'comment'  => 'Result ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'response_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'unsigned' => true,
+            'nullable' => true,
+            'comment'  => 'Response ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'protocol_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => true,
+            'comment'  => 'Protocol ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'receipt_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => true,
+            'comment'  => 'Receipt ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'average_id', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'length'   => 11,
+            'default'  => -1,
+            'nullable' => false,
+            'comment'  => 'Average ID',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'qr_code', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'nullable' => false,
+            'comment'  => 'QR Code',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'url_key', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'nullable' => false,
+            'comment'  => 'URL Key',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'response_application', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => true,
+            'comment'  => 'Response Application',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'response_reason', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => true,
+            'comment'  => 'Response Reason',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'response_key', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'length'   => 255,
+            'nullable' => true,
+            'comment'  => 'Response Key',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'created_at', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_DATETIME,
+            'nullable' => false,
+            'comment'  => 'Created At',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'updated_at', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_DATETIME,
+            'nullable' => true,
+            'comment'  => 'Updated At'
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'emitted_at', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_DATETIME,
+            'nullable' => true,
+            'comment'  => 'Emitted At',
+        ));
+    $installer->getConnection ()
+        ->addColumn ($table, 'response_at', array(
+            'type'     => Varien_Db_Ddl_Table::TYPE_DATETIME,
+            'nullable' => true,
+            'comment'  => 'Response At',
+        ));
+}
+
+addBrazilNfceResponseTable ($installer, Gamuza_Brazil_Helper_Data::NFCE_RESPONSE_TABLE, 'Gamuza Brazil NFC-e Response');
+
+$installer->endSetup ();
+
