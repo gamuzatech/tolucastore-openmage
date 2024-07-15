e2ce1691eda9ae34e315484c2f4fe608c6bc875f
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-07-14 23:17:06 -0300

    Atualizado modulo Gamuza_Basic: adicionado gravação do hash do ultimo backup

---
+++ app/code/local/Gamuza/Basic/Model/Backup/Observer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package     Gamuza_Basic
+ * @copyright   Copyright (c) 2024 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+/**
+ * Backup Observer
+ */
+class Gamuza_Basic_Model_Backup_Observer extends Mage_Backup_Model_Observer
+{
+    public const XML_PATH_BACKUP_LAST = 'system/backup/last';
+}
+
