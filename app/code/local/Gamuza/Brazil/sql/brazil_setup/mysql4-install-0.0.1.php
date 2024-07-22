cbbed3f55fb801da2a49b4b9072d1c3a585d7eda
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-07-22 11:21:49 -0300

    Atualizado modulo Gamuza_Brazil: downgrade para 0.0.13

--- app/code/local/Gamuza/Brazil/sql/brazil_setup/mysql4-install-0.0.1.php
+++ app/code/local/Gamuza/Brazil/sql/brazil_setup/mysql4-install-0.0.1.php
@@ -8,6 +8,9 @@
 $installer = new Mage_Customer_Model_Entity_Setup('brazil_setup');
 $installer->startSetup ();
 
+/**
+ * brazil_rg_ie
+ */
 $installer->addAttribute(
     'customer',
     Gamuza_Brazil_Helper_Data::CUSTOMER_ATTRIBUTE_BRAZIL_RG_IE,
@@ -33,6 +36,42 @@ $forms = array(
 
 $attribute = Mage::getSingleton ('eav/config')->getAttribute(
     $installer->getEntityTypeId ('customer'), Gamuza_Brazil_Helper_Data::CUSTOMER_ATTRIBUTE_BRAZIL_RG_IE)
+;
+$attribute->setData ('used_in_forms', $forms)
+    ->setData('is_system', true)
+    ->setData('sort_order', 1000)
+;
+$attribute->save ();
+
+/**
+ * brazil_ie_icms
+ */
+$installer->addAttribute(
+    'customer',
+    Gamuza_Brazil_Helper_Data::CUSTOMER_ATTRIBUTE_BRAZIL_IE_ICMS,
+    array(
+        'type'         => 'int',
+        'length'       => 11,
+        'input'        => 'select',
+        'label'        => Mage::helper ('brazil')->__('IE / ICMS'),
+        'visible'      => true,
+        'required'     => false,
+        'user_defined' => false,
+        'unique'       => false,
+        'source'       => 'brazil/eav_entity_attribute_source_ie_icms',
+    )
+);
+
+$forms = array(
+    'adminhtml_customer',
+    'adminhtml_checkout',
+    'customer_account_create',
+    'customer_account_edit',
+    'checkout_register',
+);
+
+$attribute = Mage::getSingleton ('eav/config')->getAttribute(
+    $installer->getEntityTypeId ('customer'), Gamuza_Brazil_Helper_Data::CUSTOMER_ATTRIBUTE_BRAZIL_IE_ICMS)
 ;
 $attribute->setData ('used_in_forms', $forms)
     ->setData('is_system', true)
