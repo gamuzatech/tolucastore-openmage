53245a0df7c062a0c0e99a984366196e07ce0386
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-08-02 17:13:08 -0300

    Atualizado modulo Gamuza_Brazil: adicionado suporte a NF-e

---
+++ app/code/local/Gamuza/Brazil/Block/Adminhtml/Nfe/Response.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     Gamuza_Brazil
+ * @copyright   Copyright (c) 2024 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+class Gamuza_Brazil_Block_Adminhtml_Nfe_Response extends Mage_Adminhtml_Block_Widget_Grid_Container
+{
+	public function __construct ()
+	{
+	    $this->_blockGroup = 'brazil';
+	    $this->_controller = 'adminhtml_nfe_response';
+
+	    $this->_headerText = Mage::helper ('brazil')->__('NF-e Response Manager');
+
+	    parent::__construct();
+
+        $this->_removeButton ('add');
+
+        $this->_addBackButton ();
+	}
+
+    public function getBackUrl()
+    {
+        return $this->getUrl('*/*/index');
+    }
+}
+
