d185dad44ac868a9818e2887a095871a05c74147
Eneias Ramos <eneias@gamuza.com.br>
2024-10-07 14:33:31 -0300

    Atualizado modulo Gamuza_Basic: adicionado sufixo COMANDA e SERVICE nos pedidos

--- app/code/local/Gamuza/Basic/Model/Observer.php
+++ app/code/local/Gamuza/Basic/Model/Observer.php
@@ -344,6 +344,23 @@ CONTENT;
         $quoteItem->setData (Gamuza_Basic_Helper_Data::ORDER_ITEM_ATTRIBUTE_GTIN, $productGTIN);
     }
 
+    public function salesQuoteCollectTotalsAfter ($observer)
+    {
+        $quote = $observer->getEvent ()->getQuote ();
+
+        $collection = Mage::getResourceModel ('sales/quote_item_collection')
+            ->setQuote ($quote)
+            ->addFieldToFilter ('product_type', Gamuza_Basic_Model_Catalog_Product_Type_Service::TYPE_SERVICE)
+        ;
+
+        if ($collection->count() > 0)
+        {
+            $quote->setData (Gamuza_Basic_Helper_Data::ORDER_ATTRIBUTE_IS_SERVICE, true)->save ();
+        }
+
+        return $this;
+    }
+
     public function salesOrderPreparingAfter ($observer)
     {
         $this->_updateOrderServiceState (
