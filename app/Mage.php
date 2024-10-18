ed8468eb685fbf492ffe8a45eaacd77a7cf4f49f
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-10-18 02:00:13 -0300

    Atualizado modulo Gamuza_Basic alterado tolucastore_version 20.10.2 => 20.10.2.1 ( temporaria ) no apiPath basic_magento.info

--- app/Mage.php
+++ app/Mage.php
@@ -215,7 +215,7 @@ final class Mage
                 'minor'     => '12',
                 'patch'     => '0',
                 'stability' => '', // beta,alpha,rc
-                'number'    => '', // 1,2,3,0.3.7,x.7.z.92 @see https://semver.org/#spec-item-9
+                'number'    => '1', // 1,2,3,0.3.7,x.7.z.92 @see https://semver.org/#spec-item-9
             ];
         }
 
