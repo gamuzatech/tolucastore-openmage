6635e5b43f3f765599fdf51fb820de017ddda5cb
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-10-18 02:23:15 -0300

    Atualizado modulo Gamuza_Basic: adicionado rotina SQL para ajustar campo scope na tabela core_config_data

--- app/code/local/Gamuza/Basic/sql/basic_setup/mysql4-upgrade-0.0.13-0.0.14.php
+++ app/code/local/Gamuza/Basic/sql/basic_setup/mysql4-upgrade-0.0.13-0.0.14.php
@@ -8,6 +8,20 @@
 $installer = new Mage_Core_Model_Resource_Setup ('basic_setup');
 $installer->startSetup ();
 
+function updateCoreConfigDataTable ($installer, $model, $comment)
+{
+    $table = $installer->getTable ($model);
+
+$updateCoreConfigDataQuery = <<< QUERY
+    ALTER TABLE {$table} MODIFY COLUMN scope varchar(255) DEFAULT 'default' NOT NULL COMMENT 'Config Scope'
+QUERY;
+
+    Mage::getSingleton ('core/resource')
+        ->getConnection ('core_write')
+        ->query ($updateCoreConfigDataQuery)
+    ;
+}
+
 function updateCoreSessionTable ($installer, $model, $comment)
 {
     $table = $installer->getTable ($model);
@@ -50,6 +64,7 @@ QUERY;
     ;
 }
 
+updateCoreConfigDataTable ($installer, 'core_config_data', 'Config Data');
 updateCoreSessionTable ($installer, 'core_session', 'Session ID');
 updateAPISessionTable ($installer, 'api_session', 'Session ID');
 updateLogVisitorTable ($installer, 'log_visitor', 'Session ID');
