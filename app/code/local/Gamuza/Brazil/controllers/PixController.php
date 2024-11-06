8570bdeb1ac832c4c30d455fe62e2843f452fb76
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-11-06 13:25:13 -0300

    Atualizado modulo Gamuza_Brazil: adicionado exibição de chave e qrcode PIX no pedido

---
+++ app/code/local/Gamuza/Brazil/controllers/PixController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     Gamuza_Brazil
+ * @copyright   Copyright (c) 2024 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+class Gamuza_Brazil_PixController extends Mage_Core_Controller_Front_Action
+{
+    use Gamuza_Brazil_Trait_Controller_Pix;
+}
+
