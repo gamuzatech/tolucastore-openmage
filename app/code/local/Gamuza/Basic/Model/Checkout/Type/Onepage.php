b375992112af473dc1df20b6ab18ce7dc65e5575
Eneias Ramos de Melo <eneias@gamuza.com.br>
2025-04-30 17:30:37 -0300

    Atualizado modulo Gamuza_Basic: adicionado rotina para gerar email no checkout

---
+++ app/code/local/Gamuza/Basic/Model/Checkout/Type/Onepage.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package     Gamuza_Basic
+ * @copyright   Copyright (c) 2025 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+/**
+ * One page checkout processing model
+ */
+class Gamuza_Basic_Model_Checkout_Type_Onepage extends Mage_Checkout_Model_Type_Onepage
+{
+    /**
+     * Save billing address information to quote
+     * This method is called by One Page Checkout JS (AJAX) while saving the billing information.
+     *
+     * @param array $data
+     * @param int $customerAddressId
+     * @return array|true
+     * @throws Mage_Core_Exception
+     */
+    public function saveBilling($data, $customerAddressId)
+    {
+        if (!array_key_exists('email', $data) && Mage::getStoreConfigFlag(Gamuza_Basic_Model_Customer_Customer::XML_PATH_GENERATE_HUMAN_FRIENDLY_EMAIL))
+        {
+            $customerCode   = hash('md5', uniqid(rand(), true));
+            $customerDomain = Mage::getStoreConfig(Mage_Customer_Model_Customer::XML_PATH_DEFAULT_EMAIL_DOMAIN);
+            $customerEmail  = sprintf('%s@%s', $customerCode, $customerDomain);
+
+            $data['email'] = $customerEmail;
+        }
+
+        return parent::saveBilling($data, $customerAddressId);
+    }
+}
+
