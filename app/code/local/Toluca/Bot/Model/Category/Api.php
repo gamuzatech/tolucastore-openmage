083e7e84ecd07a048ba9916cbb878b9ec782a999
Eneias Ramos <eneias@gamuza.com.br>
2025-08-28 21:00:01 -0300

    Atualizado modulo Toluca_Bot: adicionado apiPath bot_category.list

---
+++ app/code/local/Toluca/Bot/Model/Category/Api.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package     Toluca_Bot
+ * @copyright   Copyright (c) 2025 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+/**
+ * Category API
+ */
+class Toluca_Bot_Model_Category_Api extends Toluca_Bot_Model_Api_Resource_Abstract
+{
+    public function items ()
+    {
+        Mage::app ()->setCurrentStore (Mage_Core_Model_App::DISTRO_STORE_ID);
+
+        $storeId = Mage::app ()->getStore ()->getId ();
+
+        $result = $this->_getCategoryList ($storeId);
+
+        return $result;
+    }
+}
+
