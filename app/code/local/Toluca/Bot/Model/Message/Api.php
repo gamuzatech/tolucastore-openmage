55e03b7a25123efe8a2486bf9c31b86dc47cb466
Eneias Ramos <eneias@gamuza.com.br>
2025-09-29 14:07:23 -0300

    Atualizado modulo Toluca_Bot: adicionado apiPath bot_message.add

--- app/code/local/Toluca/Bot/Model/Message/Api.php
+++ app/code/local/Toluca/Bot/Model/Message/Api.php
@@ -69,5 +69,83 @@ class Toluca_Bot_Model_Message_Api extends Toluca_Bot_Model_Api_Resource_Abstrac
 
         return $result;
     }
+
+    public function add ($type, $text)
+    {
+        $result = null;
+
+        Mage::app ()->setCurrentStore (Mage_Core_Model_App::DISTRO_STORE_ID);
+
+        $storeId = Mage::app ()->getStore ()->getId ();
+
+        $headers = Mage::helper ('bot')->headers ();
+
+        list ($botType, $from, $to, $senderName, $senderMessage) = array_values ($headers);
+
+        $quote = $this->_getQuote ($botType, $from, $to, $senderName, $senderMessage);
+
+        $from = preg_replace ('[\D]', null, $from);
+        $to   = preg_replace ('[\D]', null, $to);
+
+        $collection = Mage::getModel ('bot/chat')->getCollection ()
+            ->addFieldToFilter ('store_id',  array ('eq' => $storeId))
+            ->addFieldToFilter ('quote_id',  array ('gt' => 0))
+            ->addFieldToFilter ('order_id',  array ('eq' => 0))
+            ->addFieldToFilter ('type_id',   array ('eq' => $botType))
+            ->addFieldToFilter ('number',    array ('eq' => $from))
+            ->addFieldToFilter ('phone',     array ('eq' => $to))
+            ->addFieldToFilter ('status',    array ('neq' => Toluca_Bot_Helper_Data::STATUS_ORDER))
+        ;
+
+        $collection->getSelect ()
+            ->order ('created_at DESC')
+            ->limit (1)
+        ;
+
+        $chat = $collection->getFirstItem ();
+
+        if (!empty ($senderName))
+        {
+             $senderName = explode (' ', $senderName, 2);
+        }
+
+        if (is_array ($senderName) && count ($senderName) == 1)
+        {
+            $senderName [1] = '------';
+        }
+
+        if (!$senderName || !is_array ($senderName) || count ($senderName) != 2)
+        {
+            $senderName = array(
+                0 => Mage::helper ('bot')->__('Firstname'),
+                1 => Mage::helper ('bot')->__('Lastname'),
+            );
+        }
+
+        $remoteIp = Mage::helper ('bot')->getRemoteIp ();
+
+        if (!$collection->count ())
+        {
+            $chat = Mage::getModel ('bot/chat')
+                ->setTypeId ($botType)
+                ->setStoreId ($storeId)
+                ->setQuoteId ($quote->getId ())
+                ->setNumber ($from)
+                ->setPhone ($to)
+                ->setFirstname ($senderName [0])
+                ->setLastname ($senderName [1])
+                ->setRemoteIp ($remoteIp)
+                ->setEmail (self::DEFAULT_CUSTOMER_EMAIL)
+                ->setStatus (Toluca_Bot_Helper_Data::STATUS_CATEGORY)
+                ->setCreatedAt (date ('c'))
+                ->setUpdatedAt (date ('c'))
+                ->save ()
+            ;
+        }
+
+        $message = $this->_saveMessage ($text, $chat, $type);
+
+        return intval ($message->getId ());
+    }
 }
 
