9f875a9389f061a238322277d427069143ff7a85
Eneias Ramos de Melo <eneias@gamuza.com.br>
2026-03-26 03:37:17 -0300

    Atualizado modulo Gamuza_Mobile: adicionado apiPath mobile_customer.draft

---
+++ app/code/local/Gamuza/Mobile/Block/Adminhtml/Customer/Draft.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @package     Gamuza_Mobile
+ * @copyright   Copyright (c) 2026 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+class Gamuza_Mobile_Block_Adminhtml_Customer_Draft extends Mage_Adminhtml_Block_Template
+{
+    protected $_separator = '-'; // default
+
+    private $_fontgrow = 0;  // default
+    private $_wordwrap = 48; // default
+
+    public function __construct ()
+    {
+        parent::__construct ();
+
+        $this->_fontgrow = Mage::getStoreConfigFlag ('customer/customer_draft/font_grow');
+        $this->_wordwrap = Mage::getStoreConfigAsInt ('customer/customer_draft/word_wrap');
+    }
+
+    public function getFontGrow ()
+    {
+        return $this->_fontgrow;
+    }
+
+    public function getWordWrap ()
+    {
+        return $this->_wordwrap;
+    }
+
+    public function getLineSeparator ($title = null)
+    {
+        $result = str_repeat ($this->_separator, $this->_wordwrap);
+
+        if (empty ($title))
+        {
+            return $result;
+        }
+
+        $title = sprintf (' %s ', $title);
+
+        $position = strlen ($result) / 2 - strlen ($title) / 2;
+
+        for ($i = 0; $i < strlen ($title); $i ++)
+        {
+            $result [$i + $position] = $title [$i];
+        }
+
+        return $result;
+    }
+
+    public function getOrdersList ()
+    {
+        $collection = Mage::getModel ('sales/order')->getCollection ()
+            ->addFieldToFilter ('customer_id', array ('eq' => $this->getCustomer ()->getId ()))
+            ->addFieldToFilter ('state', array ('eq' => Mage_Sales_Model_Order::STATE_NEW))
+        ;
+
+        $collection->getSelect ()
+            ->join (
+                array ('sfop' => Mage::getSingleton ('core/resource')->getTableName ('sales/order_payment')),
+                'main_table.entity_id = sfop.parent_id',
+                array (
+                    'deferred_fee_percentage',
+                    'deferred_fee_amount',
+                )
+            )
+            ->order ('entity_id DESC')
+        ;
+
+        return $collection;
+    }
+
+    public function getOrdersTotal ($orders, $field)
+    {
+        $result = array_sum (array_map (function ($_order) use ($field) {
+            return $_order->getData ($field);
+        }, $orders->getItems ()));
+
+        return $result;
+    }
+
+    public function getOrderNumber ($order)
+    {
+        return sprintf ('%s', $order->getRealOrderId () ?? $order->getId ());
+    }
+
+    public function getOrderDatetime ($order)
+    {
+        return Mage::helper ('core')->formatDate ($order->getCreatedAtDate (), 'medium', true);
+    }
+
+    public function getOrderSequence ($order)
+    {
+        $result = null;
+
+        if (Mage::helper ('core')->isModuleEnabled ('Toluca_PDV'))
+        {
+            $result = $order->getData (Toluca_PDV_Helper_Data::ORDER_ATTRIBUTE_PDV_SEQUENCE_ID);
+        }
+
+        return $result ?? $order->getId ();
+    }
+}
+
---
+++ app/code/local/Gamuza/Mobile/Model/Customer/Api.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package     Gamuza_Mobile
+ * @copyright   Copyright (c) 2026 Gamuza Technologies (https://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+/**
+ * Customer api
+ */
+class Gamuza_Mobile_Model_Customer_Api extends Mage_Customer_Model_Customer_Api
+{
+    /**
+     * Draft customer
+     *
+     * @param int $customerId
+     * @return string|null
+     */
+    public function draft ($customerId)
+    {
+        $customer = Mage::getModel ('customer/customer')->load ($customerId);
+
+        if (!$customer || !$customer->getId ())
+        {
+            $this->_fault ('not_exists');
+        }
+
+        $result = Mage::app ()
+            ->getLayout ()
+            ->createBlock ('mobile/adminhtml_customer_draft')
+            ->setArea (Mage_Core_Model_App_Area::AREA_ADMINHTML)
+            ->setCustomer ($customer)
+            ->setTemplate ('gamuza/mobile/customer/draft.phtml')
+            ->toHtml ();
+
+        return $result;
+    }
+}
+
