c7dcdf90b11ccbd37d13dd5e1f97f11f35613cb8
Eneias Ramos de Melo <eneias@eneias-macbookpro.local>
2026-04-07 18:57:08 -0300

    Atualizado modulo Gamuza_Basic: adicionado suporte a pigz no backup

--- app/code/local/Gamuza/Basic/Model/Backup/Backup.php
+++ app/code/local/Gamuza/Basic/Model/Backup/Backup.php
@@ -12,5 +12,63 @@ class Gamuza_Basic_Model_Backup_Backup extends Mage_Backup_Model_Backup
 {
     /* internal constants */
     public const COMPRESS_RATE = 6;
+
+    public const XML_PATH_GLOBAL_RESOURCES_DEFAULT_SETUP_CONNECTION = 'global/resources/default_setup/connection';
+
+    public function _write ($text, $filename)
+    {
+        $tempname = tempnam (sys_get_temp_dir (), 'backup_');
+
+        file_put_contents ($tempname, $text);
+
+        $command = sprintf(
+            " cat %s | pigz -c >> %s ",
+            escapeshellarg ($tempname),
+            escapeshellarg ($filename)
+        );
+
+        $result = exec ($command, $output, $code);
+
+        if ($code != 0)
+        {
+            Mage::exception ('Mage_Backup', Mage::helper ('backup')->__('An error occurred while writing to the backup file "%s".', $filename));
+        }
+
+        unlink ($tempname);
+
+        return $this;
+    }
+
+    public function _dump ($tableName, $filename)
+    {
+        $xml = Mage::getConfig ()->getNode (self::XML_PATH_GLOBAL_RESOURCES_DEFAULT_SETUP_CONNECTION);
+
+        $sockname = strval ($xml->unix_socket);
+        $username = strval ($xml->username);
+        $password = strval ($xml->password);
+        $database = strval ($xml->dbname);
+
+        $command = sprintf(
+            ' mariadb-dump ' .
+            ' --skip-comments ' .
+            ' --single-transaction --quick --skip-extended-insert --routines --triggers --hex-blob ' .
+            " --socket=%s --user=%s --password=%s %s %s " .
+            " | sed -E '/^\/\*!4[0-9]{4} SET /d' " .
+            " | pigz -c >> %s ",
+            escapeshellarg ($sockname),
+            escapeshellarg ($username),
+            escapeshellarg ($password),
+            escapeshellarg ($database),
+            escapeshellarg ($tableName),
+            escapeshellarg ($filename)
+        );
+
+        $result = exec ($command, $output, $code);
+
+        if ($code != 0)
+        {
+            Mage::throwException (sprintf ('mariadb-dump ERROR %d', $code));
+        }
+    }
 }
 
--- app/code/local/Gamuza/Basic/Model/Backup/Db.php
+++ app/code/local/Gamuza/Basic/Model/Backup/Db.php
@@ -16,5 +16,60 @@ class Gamuza_Basic_Model_Backup_Db extends Mage_Backup_Model_Db
      *
      */
     public const BUFFER_LENGTH = parent::BUFFER_LENGTH * parent::BUFFER_LENGTH;
+
+    public function createBackup (Mage_Backup_Model_Backup $backup)
+    {
+        $backup->open (true);
+        $backup->close ();
+        $backup->deleteFile ();
+
+        $filename = $backup->getPath () . DS . $backup->getFileName ();
+
+        try
+        {
+            $backup->_write ($this->getResource ()->getHeader (), $filename);
+
+            $tables = $this->getResource ()->getTables ();
+
+            $ignoreDataTablesList = $this->getIgnoreDataTablesList ();
+
+            foreach ($tables as $tableName)
+            {
+                /*
+                $backup->_write ($this->getResource ()->getTableHeader ($tableName), $filename);
+                */
+
+                $tableStatus = $this->getResource ()->getTableStatus ($tableName, $tables);
+
+                /*
+                if ($tableStatus->getRows () && !in_array ($tableName, $ignoreDataTablesList))
+                */
+                {
+                    /*
+                    $backup->_write ($this->getResource ()->getTableDataBeforeSql ($tableName), $filename);
+                    */
+
+                    $backup->_dump ($tableName, $filename);
+
+                    /*
+                    $backup->_write ($this->getResource ()->getTableDataAfterSql ($tableName), $filename);
+                    */
+                }
+            }
+
+            /*
+            $backup->_write ($this->getResource ()->getTableForeignKeysSql (), $filename);
+            */
+            $backup->_write ($this->getResource ()->getFooter (), $filename);
+
+            clearstatcache (true, $filename);
+        }
+        catch (Exception $e)
+        {
+            throw $e;
+        }
+
+        return $this;
+    }
 }
 
