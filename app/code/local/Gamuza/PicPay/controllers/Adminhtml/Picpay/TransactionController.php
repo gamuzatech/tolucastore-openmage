5ab94372e110dc5eb627447d635cc73674a446f7
Eneias Ramos de Melo <eneias@eneias-MacBookPro.local>
2026-08-11 12:25:51 -0300

    Atualizado modulo Gamuza_PicPay: ajustado rotas para adminhtml

--- app/code/local/Gamuza/PicPay/controllers/Adminhtml/Picpay/TransactionController.php
+++ app/code/local/Gamuza/PicPay/controllers/Adminhtml/Picpay/TransactionController.php
@@ -5,7 +5,7 @@
  * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
  */
 
-class Gamuza_PicPay_Adminhtml_TransactionController extends Mage_Adminhtml_Controller_Action
+class Gamuza_PicPay_Adminhtml_Picpay_TransactionController extends Mage_Adminhtml_Controller_Action
 {
 	protected function _isAllowed ()
 	{
