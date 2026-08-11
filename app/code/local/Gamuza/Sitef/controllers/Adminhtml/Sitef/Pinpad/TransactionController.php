ae9bc88e4feb978443bd79df40f1a7ee77dffb9c
Eneias Ramos de Melo <eneias@eneias-MacBookPro.local>
2026-08-11 12:29:40 -0300

    Atualizado modulo Gamuza_Sitef: ajustado rotas para adminhtml

--- app/code/local/Gamuza/Sitef/controllers/Adminhtml/Sitef/Pinpad/TransactionController.php
+++ app/code/local/Gamuza/Sitef/controllers/Adminhtml/Sitef/Pinpad/TransactionController.php
@@ -5,7 +5,7 @@
  * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
  */
 
-class Gamuza_Sitef_Adminhtml_Pinpad_TransactionController extends Mage_Adminhtml_Controller_Action
+class Gamuza_Sitef_Adminhtml_Sitef_Pinpad_TransactionController extends Mage_Adminhtml_Controller_Action
 {
 	protected function _isAllowed ()
 	{
