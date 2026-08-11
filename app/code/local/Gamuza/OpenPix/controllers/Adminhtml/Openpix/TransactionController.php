d72b0bc3379ce67d52d78a1e5fbef8d73cd8273e
Eneias Ramos de Melo <eneias@eneias-MacBookPro.local>
2026-08-11 12:16:08 -0300

    Atualizado modulo Gamuza_OpenPix: ajustado rotas para adminhtml

--- app/code/local/Gamuza/OpenPix/controllers/Adminhtml/Openpix/TransactionController.php
+++ app/code/local/Gamuza/OpenPix/controllers/Adminhtml/Openpix/TransactionController.php
@@ -5,7 +5,7 @@
  * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
  */
 
-class Gamuza_OpenPix_Adminhtml_TransactionController extends Mage_Adminhtml_Controller_Action
+class Gamuza_OpenPix_Adminhtml_Openpix_TransactionController extends Mage_Adminhtml_Controller_Action
 {
 	protected function _isAllowed ()
 	{
